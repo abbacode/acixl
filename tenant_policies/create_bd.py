8de011363ee94960d0b7b9bc3bf729351345d785
abbacode <mrabba@gmail.com>
2017-10-02 22:30:28 +1100

    Removed more DataNitro code

--- tenant_policies/create_bd.py
+++
@@ -1,4 +0,0 @@
-sys.path.insert(1, os.path.join(sys.path[0], '..'))
-from aci import create_all_bds
-
-create_all_bds()
--- tenant_policies/create_subnet.py
+++
@@ -1,4 +0,0 @@
-sys.path.insert(1, os.path.join(sys.path[0], '..'))
-from aci import create_all_subnets
-
-create_all_subnets()
